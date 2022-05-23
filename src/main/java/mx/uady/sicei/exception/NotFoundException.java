33b9adf46b5ac43d0e25e5ddc1de22f0ebaa88ee
Ariel May <aemd199711@gmail.com>
2022-05-22 20:11:01 -0500

    fix: exceptions

--- src/main/java/mx/uady/sicei/exception/NotFoundException.java
+++ src/main/java/mx/uady/sicei/exception/NotFoundException.java
@@ -3,7 +3,7 @@ package mx.uady.sicei.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 
    public NotFoundException(String message) {
--- src/main/java/mx/uady/sicei/exception/UploadException.java
+++ src/main/java/mx/uady/sicei/exception/UploadException.java
@@ -2,6 +2,10 @@ package mx.uady.sicei.exception;
 
 import com.amazonaws.AmazonServiceException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNPROCESSABLE_ENTITY)
 public class UploadException extends AmazonServiceException {
 
     public UploadException(String errorMessage) {
